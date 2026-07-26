8e9f81925db3f6900a2ad28c0a9566b445c238c8
Rosario Cal <tuemail@gmail.com>
2026-07-26 12:28:42 +0200

    ...

--- app/dashboard/sedes/sedes.ts
+++ app/dashboard/sedes/sedes.ts
@@ -7,8 +7,20 @@ export const sedes = [
   },
   {
     nombre: "Fuente Alta",
-    direccion: "Calle Fuente Alta, Madrid",
+    direccion: "Calle Pegaso 32 1º-1, Madrid",
     imagen: "/sedes/fuentealta.jpg",
     maps: "https://maps.google.com",
+  },
+  {
+    nombre: "Henares",
+    direccion: "Calle Fderica Montseny 23, San Fernando de Henares",
+    imagen: "/sedes/henares.jpg",
+    maps: "https://www.google.com",
+  },
+  {
+    nombre: "Peñalén",
+    direccion: "Calle Octavio Paz 19, Alcalá de Henares",
+    imagen: "/sedes/peñalen.jpg",
+    maps: "https://www.google.com",
   },
 ]
